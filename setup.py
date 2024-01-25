1d3aff8fcf06a9b019fd17096117aa820e4e9a60
jmiante <jonathasme@gmail.com>
2024-01-24 21:56:36 -0300

    collectionpy

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='pylib',
+    name='collectionpy',
     version='0.0.0',
-    description='PyLib is more than just a library; it is a comprehensive tool that provides efficient solutions for various areas, ranging from date manipulation and text formatting to the creation of impressive graphics.',
+    description='"collectionpy" is more than just a library; it is a comprehensive tool that provides efficient solutions for various areas, ranging from date manipulation and text formatting to the creation of impressive graphics.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['pylib', 'pylib.chart', 'pylib.date', 'pylib.math', 'pylib.text'],
+    packages=['collectionpy', 'collectionpy.chart', 'collectionpy.date', 'collectionpy.math', 'collectionpy.text'],
     install_requires=[],
 )
